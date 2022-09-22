e46062b0552b7b312f9eac5e4d2864d150fd3cf1
Phil2k19 <45975113+Phil2k19@users.noreply.github.com>
2022-09-22 17:45:14 -0400

    Added new files.

---
+++ Phil/MovieGridCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieGridCell.swift
+//  Phil
+//
+//  Created by Phillip Liberal on 9/20/22.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
